5b56d2d867e73e27eb487c407aab96c9312ab3e8
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-07-22 00:57:03 +0300

    python day 1

--- 2022/Day1/main.py
+++ 2022/Day1/main.py
@@ -1,1 +1,18 @@
-print("hello")
+input = open("input.txt")
+elves = []
+elf = []
+for line in input:
+    if line == "\n":
+        elves.append(elf)
+        elf = []
+    elf.append(line.strip())
+elves.append(elf)
+
+calories = []
+for elf in elves:
+    sum = 0
+    for snack in elf:
+        if snack != '':
+            sum += int(snack)
+    calories.append(sum)
+print(max(calories))
